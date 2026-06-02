1e9dc3e4728474a53a0ed5b27f4660557ec50090
yqy <yangqingyu4@gmail.com>
2026-06-02 08:42:29 +0800

    v0.3

--- on_policy_distillation.sh
+++ on_policy_distillation.sh
@@ -244,7 +244,7 @@ python3 -m verl.trainer.main_ppo \
     custom_reward_function.name=reward_func \
     trainer.val_before_train=False \
     trainer.log_val_generations=2 \
-    trainer.logger=['console','swanlab'] \
+    trainer.logger=['console','tensorboard'] \
     trainer.project_name=$PROJECT_NAME \
     trainer.experiment_name=$EXPERIMENT_NAME \
     trainer.validation_data_dir=validation_log/$EXPERIMENT_NAME \
